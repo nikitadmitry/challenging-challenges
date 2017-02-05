2735c20045f6b45592e5cc5154b8c7add78edb84
nikit <nikit@NIKITA-PC>
2017-02-05 16:09:33 +0300

    nav bar

--- Presentation/Presentation.Web/ClientApp/app/auth/auth.component.ts
+++ Presentation/Presentation.Web/ClientApp/app/auth/auth.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
     selector: 'auth',
-    template: require('./auth.component.html')
+    template: require('./auth.component.html'),
+    providers: [AuthService]
 })
 export class AuthComponent {
+    constructor(private authService: AuthService) { }
+
+    public isLoggedOn(): boolean {
+        return this.authService.isLoggedOn();
+    }
 }
--- Presentation/Presentation.Web/ClientApp/app/navmenu/navmenu.component.ts
+++ Presentation/Presentation.Web/ClientApp/app/navmenu/navmenu.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'nav-menu',
     template: require('./navmenu.component.html'),
-    styles: [require('./navmenu.component.css')]
+    styles: [require('./navmenu.component.css')],
+    encapsulation: ViewEncapsulation.None
 })
 export class NavMenuComponent {
 }
